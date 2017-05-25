c58f39c31011fff4fa7a4e3d7e511c070fdf5b97
Giovani Guizzo <Giovani Guizzo@DESKTOP-Q5LQP6V>
2017-05-25 18:26:10 +0100

    Corrected a bug when executing Selective Mutation in the testing phase.

--- src/main/java/br/ufpr/inf/gres/sentinel/main/cli/SentinelTesting.java
+++ src/main/java/br/ufpr/inf/gres/sentinel/main/cli/SentinelTesting.java
@@ -1,5 +1,6 @@
 package br.ufpr.inf.gres.sentinel.main.cli;
 
+import br.ufpr.inf.gres.sentinel.base.mutation.Operator;
 import br.ufpr.inf.gres.sentinel.base.mutation.Program;
 import br.ufpr.inf.gres.sentinel.grammaticalevolution.algorithm.problem.impl.MutationStrategyGenerationProblem;
 import br.ufpr.inf.gres.sentinel.grammaticalevolution.algorithm.representation.VariableLengthSolution;
@@ -7,6 +8,7 @@ import br.ufpr.inf.gres.sentinel.gson.GsonUtil;
 import br.ufpr.inf.gres.sentinel.gson.ResultWrapper;
 import br.ufpr.inf.gres.sentinel.integration.IntegrationFacade;
 import br.ufpr.inf.gres.sentinel.integration.IntegrationFacadeFactory;
+import br.ufpr.inf.gres.sentinel.integration.pit.PITFacade;
 import br.ufpr.inf.gres.sentinel.main.cli.args.TestingArgs;
 import com.beust.jcommander.internal.Lists;
 import com.google.common.base.Stopwatch;
@@ -14,15 +16,12 @@ import com.google.common.collect.ListMultimap;
 import com.google.common.io.Files;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.uma.jmetal.util.SolutionListUtils;
@@ -156,36 +155,57 @@ public class SentinelTesting {
     }
 
     private static ResultWrapper runSelectiveMutation(MutationStrategyGenerationProblem problem, TestingArgs testingArgs) {
-        List<VariableLengthSolution<Integer>> solutions = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            VariableLengthSolution<Integer> solution = problem.createSolution();
-            solution.clearVariables();
+        try {
+            List<VariableLengthSolution<Integer>> solutions = new ArrayList<>();
+            for (int i = 0; i < 10; i++) {
+                VariableLengthSolution<Integer> solution = problem.createSolution();
+                solution.clearVariables();
 
-            List<Integer> variables = Lists.newArrayList(
-                    0, 2, 1, 0, 0, 1, 9, // Operator Execution
-                    0, 1, 0, 0, 0, 0, 1, 1, i, // Operator and Mutants Discard
-                    3 // Store Mutants
-            );
-            solution.addAllVariables(variables);
-            solution.setAttribute("Name", "SM_" + (i + 1) + "");
-            solutions.add(solution);
+                List<Integer> variables = Lists.newArrayList(
+                        0, 1, 0, 0, 0, 0, 1, 1, i, // Operator and Mutants Discard
+                        0, 2, 1, 0, 0, 1, 9, // Operator Execution
+                        3 // Store Mutants
+                );
+                solution.addAllVariables(variables);
+                solution.setAttribute("Name", "SM_" + (i + 1) + "");
+                solutions.add(solution);
+            }
+
+            Field allOperatorsField = PITFacade.class.getDeclaredField("ALL_OPERATORS");
+            allOperatorsField.setAccessible(true);
+            ArrayList<Operator> allOperators = (ArrayList<Operator>) allOperatorsField.get(allOperatorsField);
+
+            IntegrationFacade facade = IntegrationFacade.getIntegrationFacade();
+            for (String testingProgram : testingArgs.testingPrograms) {
+                facade.initializeConventionalStrategy(facade.instantiateProgram(testingProgram), testingArgs.numberOfTestingRuns);
+                facade.executeOperators(allOperators);
+            }
+
+            Stopwatch stopwatch = Stopwatch.createStarted();
+            for (VariableLengthSolution<Integer> solution : solutions) {
+                problem.evaluateNoConstraints(solution);
+            }
+            stopwatch.stop();
+
+            solutions.sort(Comparator.comparingDouble(o -> o.getObjective(1)));
+            ResultWrapper resultWrapper = new ResultWrapper()
+                    .setExecutionTimeInMillis(stopwatch.elapsed(TimeUnit.MILLISECONDS))
+                    .setResult(solutions)
+                    .setGrammarFile(testingArgs.grammarFilePath)
+                    .setSession("SelectiveMutation")
+                    .setObjectiveFunctions(testingArgs.objectiveFunctions)
+                    .setRunNumber(1);
+            return resultWrapper;
+        } catch (NoSuchFieldException ex) {
+            Logger.getLogger(SentinelTesting.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SecurityException ex) {
+            Logger.getLogger(SentinelTesting.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(SentinelTesting.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(SentinelTesting.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-        Stopwatch stopwatch = Stopwatch.createStarted();
-        for (VariableLengthSolution<Integer> solution : solutions) {
-            problem.evaluateNoConstraints(solution);
-        }
-        stopwatch.stop();
-
-        solutions.sort(Comparator.comparingDouble(o -> o.getObjective(1)));
-        ResultWrapper resultWrapper = new ResultWrapper()
-                .setExecutionTimeInMillis(stopwatch.elapsed(TimeUnit.MILLISECONDS))
-                .setResult(solutions)
-                .setGrammarFile(testingArgs.grammarFilePath)
-                .setSession("SelectiveMutation")
-                .setObjectiveFunctions(testingArgs.objectiveFunctions)
-                .setRunNumber(1);
-        return resultWrapper;
+        return null;
     }
 
     private static List<VariableLengthSolution<Integer>> getNonDominatedSolutions(Collection<ResultWrapper> resultsFromJson) {
